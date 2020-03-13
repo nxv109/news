d7e4c2ffa885581d2a2ee6c702527cab615745f2
vinh <nxv109@gmail.com>
2020-03-13 15:52:07 +0700

    make it better

--- server.js
+++ server.js
@@ -1,32 +1,31 @@
 const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
-const cookieParser = require("cookie-parser");
 const fileUpload = require("express-fileupload");
-const logger = require("morgan");
 const cateNewsRouter = require("./routes/cateNews");
 const newsRouter = require("./routes/news");
 const loginRouter = require("./routes/login");
 const newsEditRouter = require("./routes/newsEditor");
 const newsSensorRouter = require("./routes/newsSensor");
-const likeRouter = require('./routes/like');
-const rateRouter = require('./routes/rate');
-const usersRouter = require('./routes/users');
-const followersRouter = require('./routes/follow');
-const statisticalRouter = require('./routes/statistical');
-const commentRouter = require('./routes/comment');
+const likeRouter = require("./routes/like");
+const rateRouter = require("./routes/rate");
+const usersRouter = require("./routes/users");
+const followersRouter = require("./routes/follow");
+const statisticalRouter = require("./routes/statistical");
+const commentRouter = require("./routes/comment");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const app = express();
-rateRouter
+
 app.use(fileUpload());
 dotenv.config();
-app.use(cookieParser());
 // view engine setup
-app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(
+  express.urlencoded({
+    extended: false
+  })
+);
 
 app.use(function(req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -55,30 +54,25 @@ const connectMongoDB = async () => {
 };
 connectMongoDB();
 
-app.use('/users', usersRouter);
+app.use("/users", usersRouter);
 app.use("/cateNews", cateNewsRouter);
 app.use("/news", newsRouter);
 app.use("/newsEdits", newsEditRouter);
 app.use("/newsSensors", newsSensorRouter);
 app.use("/login", loginRouter);
-app.use('/likes', likeRouter);
-app.use('/rates', rateRouter);
-app.use('/statisticals', statisticalRouter);
-app.use('/followers', followersRouter);
-app.use('/comments', commentRouter);
+app.use("/likes", likeRouter);
+app.use("/rates", rateRouter);
+app.use("/statisticals", statisticalRouter);
+app.use("/followers", followersRouter);
+app.use("/comments", commentRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
-const port  = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started at port: ${port}`));
